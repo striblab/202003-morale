874b3e969de8b8eb39b36ffa9bbc14884e38cea4
anna boone <anna.boone@startribune.com>
2020-04-15 14:32:35 -0500

    from grid to flex

--- src/Audio.svelte
+++ src/Audio.svelte
@@ -7,14 +7,13 @@ export let booster;
 {#if booster.from_strib === "TRUE"}
 
 <div class="card strib {booster.type}">
-
+	<h5 class="stamp">{booster.timestamp} • Star Tribune</h5>
 	<audio controls>
 		<source src="{booster.asset}"/>
 	</audio>
 	<div class="text">
-		<h5 class="stamp">{booster.timestamp}</h5>
 		<p>{booster.story}</p>
-		<h4 class="author">— {booster.name}</h4>
+		<a href="{booster.url}" target="_blank"><h4 class="author">Read more</h4></a>
 	</div>
 
 </div>
